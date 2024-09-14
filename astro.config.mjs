9825c557af0c3327e691096c97a6e37cd4ff4717
twodft <64566069+twodft@users.noreply.github.com>
2024-09-14 13:50:48 +0800

    bug: reporduction the bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,35 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
+import { defineConfig } from 'astro/config'
+// import netlify from '@astrojs/netlify'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_DEPLOY_URL =
 	process.env.CONTEXT === 'production'
 		? process.env.URL
-		: process.env.DEPLOY_PRIME_URL;
+		: process.env.DEPLOY_PRIME_URL
 
 // https://astro.build/config
 export default defineConfig({
 	site: NETLIFY_DEPLOY_URL || 'https://localhost:4321',
 	output: 'hybrid',
-	adapter: netlify(),
+	adapter: node({
+		mode: 'standalone',
+	}),
 	integrations: [
 		react(),
-		tailwind({ applyBaseStyles: true })
+		tailwind({ applyBaseStyles: true }),
+		(await import('astro-compress')).default({
+			CSS: false,
+			HTML: true,
+			Image: false,
+			JavaScript: false,
+			SVG: false,
+			Path: ['./dist/client/bug/'],
+		}),
 	],
 	devToolbar: { enabled: false },
 	experimental: {
 		serverIslands: true,
-	}
-});
+	},
+})
